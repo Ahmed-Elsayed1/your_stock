888624867cc4fddc9eb07a63c27ac124d758231d
Ahmed Almoghair <ahmed.almoghair@itargs.com>
2024-12-14 16:45:11 +0200

    workstation

--- packages/common/lib/src/models/news_model.dart
+++ packages/common/lib/src/models/news_model.dart
@@ -4,6 +4,13 @@ class NewsModel {
   String? summary;
   String? bannerImage;
 
+  NewsModel({
+    this.title,
+    this.url,
+    this.summary,
+    this.bannerImage,
+  });
+
   NewsModel.fromJson(Map<String, dynamic> json) {
     title = json['title'];
     url = json['url'];
